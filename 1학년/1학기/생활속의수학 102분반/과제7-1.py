620ad1bd5cf7a0b78c1068c160c1a7b86750e093
skchqhdpdy <aniren@debian.moe>
2024-04-08 20:02:37 +0900

    7-1

--- 1학년/1학기/생활속의수학 102분반/과제7-1.py
+++ 1학년/1학기/생활속의수학 102분반/과제7-1.py
@@ -13,7 +13,12 @@ for a in range(-4, 5):
                 if a != b and checkDigit(c) and checkDigit(d):
                     result.append([a, b, int(c), int(d)])
 
+print("\na, b, c, d \n")
 for i in result:
     print(i)
 
-input("X 눌러서 종료 또는 ENTER 키 입력")
+
+for i, data in enumerate(result):
+    print(f"\n{i + 1}번째")
+    print(f"{5 + data[2]} {5 + data[1]} {5 + data[3]}\n{5 + data[0]} 5 {5 - data[0]}\n{5 - data[3]} {5 - data[1]} {5 - data[2]}")
+input("\nX 눌러서 종료 또는 ENTER 키 입력")
